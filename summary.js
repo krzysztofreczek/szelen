5bdd440781a4460492fc1fccd9fc72ef6bdf823f
krzysztofreczek <reczek.krzysztof@gmail.com>
2020-04-30 22:42:09 +0200

    Statistics

--- summary.js
+++ summary.js
@@ -1,5 +1,7 @@
 //alert("connected")
 
+var weeklyGoal = 4
+
 var users = [
   "Basia",
   "Ania",
@@ -20,7 +22,7 @@ var colors = [
 var link = window.location.href
 user = link.split("?")[1]
 if (user == "") {
-  user = "Krzysio"
+  user = users[0]
 }
 
 function initialize() {
@@ -62,9 +64,67 @@ function getEvents(callback) {
   xhr.send()
 }
 
+var thisWeekStatistics = {
+  total: 0,
+  leaders: "",
+  leaderTotal: 0,
+}
+
+var totalStatistics = {
+  total: 0,
+  leaders: "",
+  leaderTotal: 0,
+}
+
+function analyzeData() {
+  var today = new Date()
+  var monday = getMonday(today)
+  for (var e of events) {
+    if (e.timestamp >= monday) {
+      updateStats(thisWeekStatistics, e)
+    }
+    updateStats(totalStatistics, e)
+  }
+}
+
+function updateStats(stats, e) {
+  stats.total++
+  if (!stats[e.user]) {
+    stats[e.user] = {
+      total: 0
+    }
+  }
+  stats[e.user].total++
+
+  if (stats.leaderTotal < stats[e.user].total) {
+    stats.leaderTotal = stats[e.user].total
+  }
+
+  var leaders = ""
+  for (var u of users) {
+    if (stats[u] && stats[u].total == stats.leaderTotal) {
+      if (leaders != "") {
+        leaders += ", "
+      }
+      leaders += u
+    }
+  }
+  stats.leaders = leaders
+
+}
+
 function loadPageContents() {
+  analyzeData()
+  printLabels()
   printAllCharts()
   printAllEventsTable()
+}
+
+function printLabels() {
+  document.getElementById("weekly-total").innerHTML = thisWeekStatistics.total
+  document.getElementById("weekly-leaders").innerHTML = thisWeekStatistics.leaders + " [" + thisWeekStatistics.leaderTotal + "]"
+  document.getElementById("total").innerHTML = totalStatistics.total
+  document.getElementById("leaders").innerHTML = totalStatistics.leaders + " [" + totalStatistics.leaderTotal + "]"
 }
 
 function printAllCharts() {
@@ -74,11 +134,23 @@ function printAllCharts() {
 }
 
 function printMainWeeklyChart() {
+  var done = 0
+  var todo = weeklyGoal
+
+  if (thisWeekStatistics[user]) {
+    done = thisWeekStatistics[user].total
+    todo -= done
+  }
+
+  if (todo < 0) {
+    todo = 0
+  }
+  
   var chart = new CanvasJS.Chart("main-chart-container", {
     theme: "light2",
     animationEnabled: true,
     title: {
-      text: "Krzysio",
+      text: "Twoje",
     },
     data: [{
       type: "doughnut",
@@ -87,8 +159,8 @@ function printMainWeeklyChart() {
       indexLabel: "{label} [{y}]",
       yValueFormatString: "##0",
       dataPoints: [
-        { y: 6, label: "Zrobione", color: colors[1] },
-        { y: 1, label: "Pozostałe", color: colors[0] },
+        { y: done, label: "Zrobione", color: colors[1] },
+        { y: todo, label: "Pozostałe", color: colors[0] },
       ]
     }]
   })
@@ -96,26 +168,37 @@ function printMainWeeklyChart() {
 }
 
 function printAuxiliaryWeeklyChart() {
-  var others = [
-    "Basia",
-    "Majkel",
-    "Ania"
-  ]
   var i = 1
-  for (var o of others) {
+  for (var u of users) {
+    if (u == user) {
+      continue
+    }
+
+    var done = 0
+    var todo = weeklyGoal
+
+    if (thisWeekStatistics[u]) {
+      done = thisWeekStatistics[u].total
+      todo -= done
+    }
+
+    if (todo < 0) {
+      todo = 0
+    }
+
     var chart = new CanvasJS.Chart("chart-container-" + i, {
       theme: "light2",
       animationEnabled: true,
       title: {
-        text: o,
+        text: u,
       },
       data: [{
         type: "doughnut",
         indexLabelFontSize: 18,
         radius: 100,
         dataPoints: [
-          { y: 6, color: colors[i + 1] },
-          { y: 1, color: colors[0] },
+          { y: done, color: colors[i + 1] },
+          { y: todo, color: colors[0] },
         ]
       }]
     })
@@ -125,57 +208,52 @@ function printAuxiliaryWeeklyChart() {
 }
 
 function printHistoryChart() {
+  var today = new Date()
+  var monday = getMonday(today)
+  var mondayWeekAgo = new Date()
+  mondayWeekAgo.setDate(monday - 7)
+
+  var dataPoints = {}
+  for (var e of events) {
+    if (e.timestamp < mondayWeekAgo) {
+      continue
+    }
+
+    if (!dataPoints[e.user]) {
+      dataPoints[e.user] = []
+    }
+
+    var dataPoint = {
+      x: e.timestamp,
+      y: 1,
+    }
+    
+    dataPoints[e.user].push(dataPoint)
+  }
+
+  var i = 1
+  var data = []
+  for (var u of users) {
+    var d = {
+      type: "stackedColumn",
+      showInLegend: true,
+      color: colors[i],
+      name: u,
+      dataPoints: dataPoints[u]
+    }
+
+    data.push(d)
+    
+    i++
+  }
+
   var chart = new CanvasJS.Chart("history-chart-container", {
     animationEnabled: true,
     axisX: {
       interval: 1,
-      intervalType: "year",
+      intervalType: "day",
     },
-    data: [{
-      type: "stackedColumn",
-      showInLegend: true,
-      color: colors[1],
-      name: "Basia",
-      dataPoints: [
-        { y: 1, x: new Date(2010, 0) },
-        { y: 0, x: new Date(2011, 0) },
-        { y: 1, x: new Date(2012, 0) }
-      ]
-    },
-    {
-      type: "stackedColumn",
-      showInLegend: true,
-      color: colors[2],
-      name: "Ania",
-      dataPoints: [
-        { y: 1, x: new Date(2010, 0) },
-        { y: 1, x: new Date(2011, 0) },
-        { y: 1, x: new Date(2012, 0) }
-      ]
-    },
-    {
-      type: "stackedColumn",
-      showInLegend: true,
-      color: colors[3],
-      name: "Majkel",
-      dataPoints: [
-        { y: 1, x: new Date(2010, 0) },
-        { y: 0, x: new Date(2011, 0) },
-        { y: 0, x: new Date(2012, 0) }
-      ]
-    },
-    {
-      type: "stackedColumn",
-      showInLegend: true,
-      color: colors[4],
-      name: "Krzysio",
-      dataPoints: [
-        { y: 0, x: new Date(2010, 0) },
-        { y: 1, x: new Date(2011, 0) },
-        { y: 1, x: new Date(2012, 0) }
-      ]
-    }
-    ]
+    data: data,
   })
   chart.render()
 }
@@ -212,37 +290,58 @@ function mockEvents(callback) {
   events = [
     {
       user: "Basia",
-      timestamp: new Date(2020, 4, 28)
+      timestamp: new Date("2020/4/18")
     },
     {
       user: "Ania",
-      timestamp: new Date(2020, 4, 27)
+      timestamp: new Date("2020/4/17")
+    },
+    {
+      user: "Ania",
+      timestamp: new Date("2020/4/18")
     },
     {
       user: "Majkel",
-      timestamp: new Date(2020, 4, 28)
+      timestamp: new Date("2020/4/18")
     },
     {
       user: "Krzysio",
-      timestamp: new Date(2020, 4, 29)
+      timestamp: new Date("2020/4/26")
     },
     {
       user: "Basia",
-      timestamp: new Date(2020, 4, 30)
+      timestamp: new Date("2020/4/28")
     },
     {
       user: "Ania",
-      timestamp: new Date(2020, 4, 28)
+      timestamp: new Date("2020/4/27")
     },
     {
       user: "Majkel",
-      timestamp: new Date(2020, 4, 30)
+      timestamp: new Date("2020/4/28")
     },
     {
       user: "Krzysio",
-      timestamp: new Date(2020, 4, 30)
-    }
+      timestamp: new Date("2020/4/29")
+    },
+    {
+      user: "Basia",
+      timestamp: new Date("2020/4/30")
+    },
+    {
+      user: "Ania",
+      timestamp: new Date("2020/4/30")
+    },
+    {
+      user: "Krzysio",
+      timestamp: new Date("2020/4/30")
+    },
   ]
 
   callback()
+}
+
+function getMonday(d) {
+  var day = d.getDay(), diff = d.getDate() - day
+  return new Date(d.setDate(diff))
 }
