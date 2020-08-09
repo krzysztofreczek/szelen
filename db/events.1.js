287df58cf44f2ce29438eeeee3a0987be3f4e546
krzysztofreczek <reczek.krzysztof@gmail.com>
2020-08-09 23:23:14 +0200

    Remove test events

--- db/events.1.js
+++ db/events.1.js
@@ -3,6 +3,5 @@
 var events_1 = [
 {date:"2020/04/27",user:"Krzysio"},
 {date:"2020/08/09",user:"Ania"},
-{date:"2020/8/7",user:"Krzysio"},
 {}
 ]
--- db/events.3.js
+++ db/events.3.js
@@ -3,6 +3,5 @@
 var events_3 = [
 {date:"2020/04/27",user:"Krzysio"},
 {date:"2020/08/09",user:"Krzysio"},
-{date:"2020/8/9",user:"Krzysio"},
 {}
 ]
