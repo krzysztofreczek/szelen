f12b7dce4acd9cf0a7c59ca3c572918e408353b9
krzysztofreczek <reczek.krzysztof@gmail.com>
2021-02-08 22:43:43 +0100

    Change crossorigin proxy

--- js/events.js
+++ js/events.js
@@ -106,12 +106,12 @@ function eventsEqual(e1, e2) {
 
 function persistEvent(user, timestamp, callback) {
     var xhr = new XMLHttpRequest()
-    var url = 'https://cors-anywhere.herokuapp.com/circleci.com:443/api/v1.1/project/github/krzysztofreczek/szelen/tree/master'
+    var url = 'https://crossorigin.me/https://circleci.com:443/api/v1.1/project/github/krzysztofreczek/szelen/tree/master'
     xhr.open("POST", url, true)
 
     xhr.setRequestHeader("Authorization", "Basic " + btoa("cf6722fa167f5b50afc9f33b04c7824f04052f31:"))
     xhr.setRequestHeader("Content-type", "application/json")
-    xhr.setRequestHeader("origin", "krzysztofreczek.github.io")
+    xhr.setRequestHeader("Origin", "krzysztofreczek.github.io")
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 201) {
